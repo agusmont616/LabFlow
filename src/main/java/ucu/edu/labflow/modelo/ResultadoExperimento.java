02e3b1f91c04f5ec621774749aaa92fbcb577af7
Agustín Montaña Larralde <agustinmont2001@gmail.com>
2026-05-04 13:38:49 -0300

    Clase construida

--- src/main/java/ucu/edu/labflow/modelo/ResultadoExperimento.java
+++ src/main/java/ucu/edu/labflow/modelo/ResultadoExperimento.java
@@ -1,5 +1,21 @@
 package ucu.edu.labflow.modelo;
 
 public class ResultadoExperimento {
+    private double accuracy;
+    private double precision;
+
+    public ResultadoExperimento(double accuracy, double precision) {
+        this.accuracy = accuracy;
+        this.precision = precision;
+    }
+
+    //getters
     
+    public double getAccuracy() {
+        return accuracy;
+    }
+
+    public double getPrecision() {
+        return precision;
+    }
 }
