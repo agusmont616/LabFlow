45f8c43000129365c799c23891ebcb08c7bdccd6
Agustín Montaña Larralde <agustinmont2001@gmail.com>
2026-05-04 12:57:14 -0300

    Atributos y constructor

--- src/main/java/ucu/edu/labflow/modelo/Dataset.java
+++ src/main/java/ucu/edu/labflow/modelo/Dataset.java
@@ -1,5 +1,17 @@
 package ucu.edu.labflow.modelo;
 
 public class Dataset {
+    private int id;
+    private String nombre;
+    private int tamanio;
+    private String tipoProblema;
+
+    public Dataset(int id, String nombre, int tamanio, String tipoProb) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tamanio = tamanio;
+        this.tipoProblema = tipoProb;
+    }
+
     
 }
