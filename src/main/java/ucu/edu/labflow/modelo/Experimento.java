e6f51d07000067f12a47457677c49c5790a1c5fe
Agustín Montaña Larralde <agustinmont2001@gmail.com>
2026-05-04 13:18:15 -0300

    Atributos y constructor

--- src/main/java/ucu/edu/labflow/modelo/Experimento.java
+++ src/main/java/ucu/edu/labflow/modelo/Experimento.java
@@ -1,5 +1,15 @@
 package ucu.edu.labflow.modelo;
 
 public class Experimento {
-    
+    private int id;
+    private Dataset dataset;
+    private Modelo modelo;
+    private EstadoExperimento estado;
+
+    public Experimento(int id, Dataset dataset, Modelo modelo, EstadoExperimento estado) {
+        this.id = id;
+        this.dataset = dataset;
+        this.modelo = modelo;
+        this.estado = estado;
+    }
 }
