af4e4cdbd358111833d324f9be4cdbcc341a8140
Agustín Montaña Larralde <agustinmont2001@gmail.com>
2026-05-04 13:33:19 -0300

    Getters y setters

--- src/main/java/ucu/edu/labflow/modelo/Experimento.java
+++ src/main/java/ucu/edu/labflow/modelo/Experimento.java
@@ -5,11 +5,45 @@ public class Experimento {
     private Dataset dataset;
     private Modelo modelo;
     private EstadoExperimento estado;
+    private ResultadoExperimento resultado;
 
     public Experimento(int id, Dataset dataset, Modelo modelo, EstadoExperimento estado) {
         this.id = id;
         this.dataset = dataset;
         this.modelo = modelo;
         this.estado = estado;
+        this.resultado = null;
+    }
+
+    //getters
+
+    public int getId() {
+        return id;
+    }
+
+    public Dataset getDataset() {
+        return dataset;
+    }
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    public EstadoExperimento getEstado() {
+        return estado;
+    }
+
+    public ResultadoExperimento getResultado() {
+        return resultado;
+    }
+
+    //setters
+
+    public void setEstado(EstadoExperimento estadoNuevo) {
+        this.estado = estadoNuevo;
+    }
+
+    public void setResultado(ResultadoExperimento resultadoNuevo) {
+        this.resultado = resultadoNuevo;
     }
 }
