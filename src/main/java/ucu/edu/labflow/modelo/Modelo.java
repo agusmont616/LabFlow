55dee26cbcacc881e340a8d40e16d63ad4b234b3
Agustín Montaña Larralde <agustinmont2001@gmail.com>
2026-05-04 13:05:47 -0300

    Atributos y constructor

--- src/main/java/ucu/edu/labflow/modelo/Modelo.java
+++ src/main/java/ucu/edu/labflow/modelo/Modelo.java
@@ -4,5 +4,12 @@ public class Modelo {
     private int id;
     private String nombre;
     private String tipoModelo;
-    priva
+    private String parametros; //simulado
+
+    public Modelo(int id, String nombre, String tipoMod, String param) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipoModelo = tipoMod;
+        this.parametros = param;
+    }
 }
