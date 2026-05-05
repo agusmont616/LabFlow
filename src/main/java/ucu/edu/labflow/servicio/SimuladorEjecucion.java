1beaa765d7c25a8327ab86fc2c94879fcf968af3
Agustín Montaña Larralde <agustinmont2001@gmail.com>
2026-05-05 09:39:29 -0300

    Clase SimuladorEjecucion terminada

--- src/main/java/ucu/edu/labflow/servicio/SimuladorEjecucion.java
+++ src/main/java/ucu/edu/labflow/servicio/SimuladorEjecucion.java
@@ -1,9 +1,49 @@
 package ucu.edu.labflow.servicio;
 
+import ucu.edu.labflow.modelo.*;
+import ucu.edu.labflow.tda.implementaciones.lineales.cola.ColaEnlazada;
+import java.util.Random;
+
 public class SimuladorEjecucion {
 
     /*Requerimientos:
         1. registrar  accuracy y precision (pueden ser más)
         2. cambiar estado de experimento a ejecutado
         3. historial de experimentos*/ 
+    
+    private ColaEnlazada<Experimento> pendientes;
+    private ColaEnlazada<Experimento> historial;
+    private Random random;
+
+    public SimuladorEjecucion() {
+        this.pendientes = new ColaEnlazada<>();
+        this.historial = new ColaEnlazada<>();
+        this.random = new Random();
+    }
+
+    public void ponerEnCola(Experimento experimento) {
+        this.pendientes.poneEnCola(experimento);
+    }
+
+    public void ejecutarSiguiente() {
+        if (pendientes.esVacio()) {
+            return;
+        }
+
+        Experimento e = pendientes.quitaDeCola();
+
+        double accuracy = 0.5 + (1.0 - 0.5) * random.nextDouble(); // genera un double entre 0.5 y 1.0
+        double precision = 0.5 + (1.0 - 0.5) * random.nextDouble();
+
+        ResultadoExperimento resultado = new ResultadoExperimento(accuracy, precision);
+
+        e.setResultado(resultado);
+        e.setEstado(EstadoExperimento.EJECUTADO);
+
+        this.historial.poneEnCola(e);
+    }
+
+    public ColaEnlazada<Experimento> getHistorial() {
+        return historial;
+    }
 }
